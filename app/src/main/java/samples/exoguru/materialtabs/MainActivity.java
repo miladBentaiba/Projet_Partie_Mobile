15320f6cac65b130f8319665947460a358a1ee67
HSarah <bs_habchi@esi.dz>
2015-05-12 14:42:57 +0100

    3rd version

--- app/src/main/java/samples/exoguru/materialtabs/MainActivity.java
+++ app/src/main/java/samples/exoguru/materialtabs/MainActivity.java
@@ -116,6 +116,7 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+        
 
     }
 
